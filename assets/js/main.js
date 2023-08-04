0066a6dace202346c8023abd13192e75fd542676
Kumar, Ram (Contractor) <R_Kumar@acs.org>
2023-08-04 23:20:40 +0530

    Theme Changes

---
+++ assets/js/main.js
@@ -0,0 +1,184 @@
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    if (all) {
+      select(el, all).forEach(e => e.addEventListener(type, listener))
+    } else {
+      select(el, all).addEventListener(type, listener)
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Sidebar toggle
+   */
+  if (select('.toggle-sidebar-btn')) {
+    on('click', '.toggle-sidebar-btn', function(e) {
+      select('body').classList.toggle('toggle-sidebar')
+    })
+  }
+
+  /**
+   * Search bar toggle
+   */
+  if (select('.search-bar-toggle')) {
+    on('click', '.search-bar-toggle', function(e) {
+      select('.search-bar').classList.toggle('search-bar-show')
+    })
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Initiate tooltips
+   */
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  
+
+  /**
+   * Initiate Bootstrap validation check
+   */
+  var needsValidation = document.querySelectorAll('.needs-validation')
+
+  Array.prototype.slice.call(needsValidation)
+    .forEach(function(form) {
+      form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+
+  /**
+   * Initiate Datatables
+   */
+  const datatables = select('.datatable', true)
+  
+  /**
+   * Code Heighlighter - Copy ClipBoard
+   */
+
+  const _codes = select('.highlighter-rouge', true);
+
+  const copyClipBoardInitialize = () => {
+    const codeBlocks = document.querySelectorAll('.highlighter-rouge');
+    const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+
+    copyCodeButtons.forEach((copyCodeButton, index) => {
+    const code = codeBlocks[index].innerText;
+
+    copyCodeButton.addEventListener('click', () => {
+      // Copy the code to the user's clipboard
+      window.navigator.clipboard.writeText(code);
+
+      // (Optional) Toggle a class for styling the button
+      copyCodeButton.classList.add('copied');
+
+      // After 2 seconds, reset the button to its initial UI
+      setTimeout(() => {
+        copyCodeButton.classList.remove('copied');
+      }, 2000);
+     });
+   });
+  };
+
+  const createCopyButton = () => {
+    let _button = document.createElement('button');
+    let _wrapper = document.createElement('div');
+    let _icon = document.createElement('i');
+    _icon.className= 'fa fa-light fa-clipboard';
+    _wrapper.classList.add('code-header');
+    _button.classList.add('copy-code-button');
+    _button.appendChild(_icon);
+    _wrapper.appendChild(_button);
+    return _wrapper;
+  };
+
+  if(_codes){
+     for(var i in _codes){
+      _codes[i].insertBefore(createCopyButton(), _codes[i].children[0]);
+    }
+    copyClipBoardInitialize();
+  }
+
+})();
+
